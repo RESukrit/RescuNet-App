a36f675cf902123ca1291734df5a128778fff798
RESukrit <100398317+RESukrit@users.noreply.github.com>
2026-09-12 02:35:35 +0530

    Update LeafletMap.tsx

--- src/components/LeafletMap.tsx
+++ src/components/LeafletMap.tsx
@@ -37,8 +37,13 @@ export const LeafletMap: React.FC<LeafletMapProps> = ({
       zoomControl: true,
     });
 
-    // Dark-themed tiles for emergency command center look
-    L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
+ // Dark-themed tiles for emergency command center look
+    const cartoKey = import.meta.env.VITE_CARTO_API_KEY;
+    const tileUrl = cartoKey
+      ? `https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png?api_key=${cartoKey}`
+      : 'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png';
+
+    L.tileLayer(tileUrl, {
       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
       subdomains: 'abcd',
       maxZoom: 19,
